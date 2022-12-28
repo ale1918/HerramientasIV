a71d09e33ceaeebe07a6dca90bff592adf3d8c43
alega <alega@DESKTOP-28O0HS3>
2022-12-28 08:15:04 -0500

    Primera subida del proyecto para herramientas IV del 2022

---
+++ HerramientasIV2022/src/herramientasiv2022/HerramientasIV2022.java
@@ -0,0 +1,17 @@
+
+package herramientasiv2022;
+
+/**
+ *
+ * @author alega
+ */
+public class HerramientasIV2022 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
